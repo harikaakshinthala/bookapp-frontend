401339e28f48f76419aa18c1289c47855815e465
harikakshinthala <harikaakshinthala1@gmail.com>
2023-09-16 16:59:55 +0530

    WITHOUTBACKEND

--- src/global.js
+++ src/global.js
@@ -1,4 +1,6 @@
- export const API = "https://64eb2855e51e1e82c5770916.mockapi.io"
+export const API = "https://64eb2855e51e1e82c5770916.mockapi.io"
 
 // //for cnnection to backend
-// export const API = " http://localhost:7000"
+// export const API = " http://localhost:8000"
+
+// export const API = " https://bookapp-backend2.onrender.com"
